e9c7c1cdf8a26b3b3c2cc7e776c5bc18c25e3240
joseaugustojesus <joseaugustolimajesus@gmail.com>
2021-07-10 17:50:22 -0300

    Insert new methods;
    change method name changeEnv to change; getOne to get specific key; GetAllKeys to return all keys from dotenv; GetAllValues to return all values from dotenv; getAllKeysAndValues to return array contains keys and values from dotenv;

--- jose-augusto/app/HandleEnv.php
+++ jose-augusto/app/HandleEnv.php
@@ -9,7 +9,7 @@ class HandleEnv
      *
      * @return boolean
      */
-    public static function changeEnv($values = [], $fileDir)
+    public static function change($values = [], $fileDir)
     {
 
         if (count($values) > 0) {
@@ -48,38 +48,124 @@ class HandleEnv
     }
 
 
+
     /**
      * @param string $key
      * @param string $fileDir
      *
      * @return array
      */
-    public static function get($key, $fileDir)
+    public static function getOne($key, $fileDir)
     {
-        if (!empty($key) && $key !== "") {
+        if (!empty($key)) {
             $fileExists = file_exists($fileDir);
-            $env = file_get_contents($fileDir);
-            $env = preg_split('/\s+/', $env);
 
 
-            $value = array_filter($env,  function ($variable) use($key){
-                $xVariable = explode("=", $variable);
-                if(is_array($xVariable)){
-                    return $xVariable[0] === $key ?  true : false;
+            if ($fileExists) {
+
+                $env = file_get_contents($fileDir);
+                $env = preg_split('/\s+/', $env);
+
+                $value = array_filter($env,  function ($variable) use ($key) {
+                    $xVariable = explode("=", $variable);
+                    if (is_array($xVariable)) {
+                        return $xVariable[0] === $key ?  true : false;
+                    }
+                });
+
+                $variable = [];
+                if (count($value) > 0) {
+                    $value = explode("=", $value[0]);
+                    $variable["key"] = $value[0];
+                    $variable["value"] = $value[1];
+                } else {
+                    $variable["key"] = "This key not founded in {$fileDir}";
+                    $variable["value"] = null;
                 }
-            });
 
-            $variable = [];
-            if(count($value) > 0){
-                $value = explode("=", $value[0]);
-                $variable["key"] = $value[0];
-                $variable["value"] = $value[1];
-            }else{
-                $variable["key"] = "This key not founded in {$fileDir}";
-                $variable["value"] = null;
+                return $variable;
+            } else {
+                return "The .env file could not be found in the specified path";
             }
+        }
+    }
 
-            return $variable;
+    /**
+     * @param string $fileDir
+     *
+     * @return array|string
+     */
+    public static function getAllKeys($fileDir)
+    {
+        if (!empty($fileDir)) {
+            $fileExists = file_exists($fileDir);
+            if ($fileExists) {
+
+                $env = file_get_contents($fileDir);
+                $env = preg_split('/\s+/', $env);
+                return array_filter(array_map(function ($item) {
+                    return explode("=", $item)[0];
+                }, $env));
+            } else {
+                return "The .env file could not be found in the specified path";
+            }
+        } else {
+            return "Cannot search for keys without .env file path";
+        }
+    }
+
+   /**
+     * @param string $fileDir
+     *
+     * @return array|string
+     */
+    public static function getAllValues($fileDir)
+    {
+        if (!empty($fileDir)) {
+            $fileExists = file_exists($fileDir);
+            if ($fileExists) {
+
+                $env = file_get_contents($fileDir);
+                $env = preg_split('/\s+/', $env);
+
+                return array_filter(array_map(function ($item) {
+                    $xItem = explode("=", $item);
+                    if (count($xItem) === 2) return explode("=", $item)[1];
+                    else return [];
+                }, $env));
+            } else {
+                return "The .env file could not be found in the specified path";
+            }
+        } else {
+            return "Cannot search for keys without .env file path";
+        }
+    }
+
+    /**
+     * @param string $fileDir
+     *
+     * @return array|string
+     */
+    public static function getAllKeysAndValues($fileDir)
+    {
+        if (!empty($fileDir)) {
+            $fileExists = file_exists($fileDir);
+            if ($fileExists) {
+
+                $env = file_get_contents($fileDir);
+                $env = array_filter(preg_split('/\s+/', $env));
+
+
+                return array_filter(array_map(function ($item) {
+                    $xItem = explode("=", $item);
+                    if (count($xItem) === 2) return explode("=", $item);
+                    else return [];
+                }, $env));
+            } else {
+                return "The .env file could not be found in the specified path";
+            }
+        } else {
+            return "Cannot search for keys without .env file path";
         }
     }
 }
